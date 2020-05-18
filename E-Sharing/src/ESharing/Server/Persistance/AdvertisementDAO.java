fc7b0ae1fe1f75acdda164bcd65870990991d2c8
Estera <dumi_estera@yahoo.com>
2020-05-18 13:55:26 +0200

    advertdao

--- E-Sharing/src/ESharing/Server/Persistance/AdvertisementDAO.java
+++ E-Sharing/src/ESharing/Server/Persistance/AdvertisementDAO.java
@@ -1,4 +1,10 @@
 package ESharing.Server.Persistance;
 
+import ESharing.Shared.TransferedObject.Advertisement;
+
 public interface AdvertisementDAO {
+
+  boolean create(Advertisement advertisement);
+  boolean addAdvertisement(Advertisement advertisement);
+  boolean removeAdvertisement(Advertisement advertisement);
 }
